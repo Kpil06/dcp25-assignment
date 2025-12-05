d3edd68b94c0e056232f67bd85c7bb9ca2f83ecd
Kpil06 <C24383681@TUDublin.ie>
2025-12-05 04:00:28 +0000

    Created a new file called visuals for the graphs

---
+++ visuals.py
@@ -0,0 +1,8 @@
+"""
+Visualising the dataset using matplotlib
+imports the analysis functions that are already in the df and use them to make graphs.
+"""
+
+import matplotlib.pyplot as plt
+
+# imort the analysis functions
