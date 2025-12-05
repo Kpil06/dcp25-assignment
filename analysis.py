7fa303b96eec5830e9d5bc957b0d82cfc1a26041
Kpil06 <C24383681@TUDublin.ie>
2025-12-05 03:35:18 +0000

    Added a function that shows how many tunes belong to each type of genre

--- analysis.py
+++ analysis.py
@@ -31,3 +31,22 @@ def tunes_by_key() -> pd.DataFrame:
 
     conn.close()
     return df
+
+def tunes_by_type() -> pd.DataFrame:
+    """
+    Returns a df showing how many tunes belong to each type.
+    """
+    conn = get_connection()
+
+    df = pd.read_sql_query(
+        """
+        SELECT tune_type, COUNT(*) AS tune_count
+        FROM tunes
+        GROUP BY tune_type
+        ORDER BY tune_count DESC;
+        """,
+        conn,
+    )
+
+    conn.close()
+    return df
